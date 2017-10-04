b1e05f2782449ccad3c940625d63fe744e0f903b
Martin Bee <martbee88@gmail.com>
2017-10-04 07:27:20 -0500

    feat: add required packages to get express development underway

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "airbnb",
+  "env": {
+    "node": true,
+  },
+  "rules": {
+    "spaced-comment": 0,
+    "no-console": 0
+  },
+  "plugins": [
+    "import"
+  ],
+  "parser": "babel-eslint",
+};
