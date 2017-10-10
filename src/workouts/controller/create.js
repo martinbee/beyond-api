bd085b32c2c03a09ed55c03a792bdef12108c570
Martin Bee <martbee88@gmail.com>
2017-10-10 07:55:21 -0500

    feat: begin create workouts refactor

--- src/workouts/controller/create.js
+++ src/workouts/controller/create.js
@@ -1,3 +1,4 @@
+// refactor put all code here first and then think about extraction
 // handle errors
 // handle first workout condition
 // add workout calculations
@@ -5,37 +6,36 @@
 import mongoose from 'mongoose';
 
 import Workout from '../model';
+import liftOrder from '../data/liftOrder';
 import {
-  getNextLiftType,
-  isEndOfMesocycle,
   updateUserTrainingMax,
 } from '../utilities';
 
-export default async function create(req, res, next) {
-  const { userId } = req.body;
-  const userObjectId = mongoose.Types.ObjectId(userId);
 
-  const lastWorkoutQuery = {
-    user: userObjectId,
-  };
-  const lastWorkoutFields = {
-    liftType: 1,
-    week: 1,
-  };
-  const lastWorkoutSort = {
-    createdAt: -1,
-  };
+const isEndOfMesocycle = (week, liftType) => {
+  const isLastWeekOfCycle = week === 3;
+  const isLastLiftOfCycle = liftOrder.indexOf(liftType) === liftOrder.length - 1;
 
-  // can't sort using findOne, have to grab all, limit, then use cursor
-  // add try catch or error handler here
-  const lastWorkoutCursor = await Workout.find(lastWorkoutQuery, lastWorkoutFields)
-    .sort(lastWorkoutSort)
-    .limit(1)
-    .populate('user', 'trainingMax');
+  return isLastWeekOfCycle && isLastLiftOfCycle;
+};
 
+const getNextLiftType = (lastLiftType) => {
+  const lastLiftTypeIndex = liftOrder.indexOf(lastLiftType);
+  const restartOrder = lastLiftTypeIndex === liftOrder.length - 1;
+
+
+  const nextLiftType = liftOrder[lastLiftTypeIndex + 1];
+
+  return nextLiftType;
+};
+
+const createFirstWorkout = (userId, req, res, next) => {
+  res.send(`${userId} wants to create their first workout`);
+};
+
+const addNewWorkout = (lastWorkoutCursor, req, res, next) => {
   const lastWorkout = lastWorkoutCursor[0];
 
-  // what if first week of workout?!
   const { week, liftType, user } = lastWorkout;
 
   const trainingMax = (
@@ -46,7 +46,6 @@ export default async function create(req, res, next) {
   const liftTrainingMax = trainingMax[nextLiftType];
 
   // calculate new workouts percentages
-  // function call
 
   // create new workout and return to client
 
@@ -62,4 +61,38 @@ export default async function create(req, res, next) {
       //}
     //});
   res.send('new workout');
+};
+
+export default function create(req, res, next) {
+  const { userId } = req.body;
+  const userObjectId = mongoose.Types.ObjectId(userId);
+
+  const lastWorkoutQuery = {
+    user: userObjectId,
+  };
+  const lastWorkoutFields = {
+    liftType: 1,
+    week: 1,
+  };
+  const lastWorkoutSort = {
+    createdAt: -1,
+  };
+
+  // refactor express args into spread args? pull callback out below?
+
+  // can't sort using findOne, have to grab all, limit, then use cursor
+  // add try catch or error handler here
+  Workout.find(lastWorkoutQuery, lastWorkoutFields)
+    .sort(lastWorkoutSort)
+    .limit(1)
+    .populate('user', 'trainingMax')
+    .exec((err, lastWorkoutCursor) => {
+      const isFirstWorkout = !lastWorkoutCursor.length;
+
+      if (isFirstWorkout) {
+        createFirstWorkout(userId, req, res, next);
+      } else {
+        addNewWorkout(lastWorkoutCursor, req, res, next);
+      }
+    });
 }
